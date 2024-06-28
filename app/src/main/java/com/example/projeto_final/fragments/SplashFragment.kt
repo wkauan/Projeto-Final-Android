588d7a573a2855c5a7add8bdc3d57f9101d63968
wkauan <kauanmbomfim5@gmail.com>
2024-06-28 12:25:26 -0300

    Finalizando o projeto

--- app/src/main/java/com/example/projeto_final/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/projeto_final/fragments/SplashFragment.kt
@@ -1,34 +1,23 @@
 package com.example.projeto_final.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.projeto_final.R
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SplashFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +27,26 @@ class SplashFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init(view)
+
+        val isLogin: Boolean = mAuth.currentUser != null
+
+        val handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+
+            if (isLogin)
+                navController.navigate(R.id.action_splashFragment_to_homeFragment)
+            else
+                navController.navigate(R.id.action_splashFragment_to_signInFragment)
+
+        }, 2000)
+    }
+
+    private fun init(view: View) {
+        mAuth = FirebaseAuth.getInstance()
+        navController = Navigation.findNavController(view)
     }
 }
---
+++ app/src/main/java/com/example/projeto_final/utils/model/ToDoData.kt
@@ -0,0 +1,3 @@
+package com.example.projeto_final.utils.model
+
+data class ToDoData(var taskId:String, var task:String)
